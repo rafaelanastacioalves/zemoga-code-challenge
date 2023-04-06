6a62ea45a15c99c19ef70801f3f6457be6113772
Rafael Anastácio Alves <rafaelanastacioalves@gmail.com>
2023-04-06 03:15:44 -0300

    feat: add interface for api call to fetch posts

---
+++ app/src/main/java/com/example/rafaelanastacioalves/moby/repository/http/APIClient.kt
@@ -0,0 +1,16 @@
+package com.example.rafaelanastacioalves.moby.repository.http;
+
+
+import com.example.rafaelanastacioalves.moby.domain.entities.EntityDetails
+import com.example.rafaelanastacioalves.moby.domain.entities.Post
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface APIClient {
+
+    @GET("/posts")
+    suspend fun getMainEntityList(): List<Post>;
+
+
+}
